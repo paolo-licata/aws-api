32601d3cca4ae8823cbe9923e48245583add8737
Paolo Licata <paolo_licata@outlook.com>
2024-07-09 17:09:00 +0200

    Re-enable token authentication on movies endpoint

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ app.use(bodyParser.json());
       });
 
       //GET a list of all movies
-      app.get('/movies', (req, res) => {
+      app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
         Movies.find()
           .then((movies) => {
             res.status(201).json(movies);
