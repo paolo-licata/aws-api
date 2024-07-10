1e9e89f2ef4e25a8e56a65b7cc20d55f1d8cd437
Paolo Licata <paolo_licata@outlook.com>
2024-07-10 18:21:08 +0200

    Adding console logs to help debug the code

--- auth.js
+++ auth.js
@@ -20,6 +20,7 @@ module.exports = (router) => {
   router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
+        console.log('No user found: ', info);
         return res.status(400).json({
           message: 'Something is not right',
           user: user
