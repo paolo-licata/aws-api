bad071b5b7e947d6cc56466a638a3919f893df3f
Paolo Licata <paolo_licata@outlook.com>
2024-07-21 17:05:57 +0200

    Updated CORS policies

--- index.js
+++ index.js
@@ -14,7 +14,18 @@ const app = express();
 
 //CORS
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflix-cfoundry.netlify.app'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message = 'The CORS policy for this application does not allow access from origin' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const { check, validationResult } = require('express-validator');
 app.use(morgan('common'));
